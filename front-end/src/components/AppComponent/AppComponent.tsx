096f4ecce3e23ec02669521ce54fb17dd8faf444
shebll <shebblloll@gmail.com>
2024-08-02 22:58:34 +0300

    feat(frontend): restructured the app components

--- front-end/src/components/AppComponent/AppComponent.tsx
+++ front-end/src/components/AppComponent/AppComponent.tsx
@@ -1,13 +1,35 @@
-// import Expenses from "./components/Expenses";
+// AppComponent.tsx
+import { useState } from "react";
+import Expenses from "./components/Expenses";
 import TotalExpenses from "./components/TotalExpenses";
 import CreateExpense from "./components/Create/CreateExpense";
+import { Expense } from "@/types/Expense";
+import { useCreateExpense } from "@/hooks/CreateExpense";
 
 const AppComponent = () => {
+  const { openModel, setOpenModel } = useCreateExpense((state) => state);
+  const [editingExpense, setEditingExpense] = useState<Expense | null>(null);
+
+  const handleCloseCreateModal = () => {
+    setOpenModel();
+    setEditingExpense(null);
+  };
+
+  const handleEditExpense = (expense: Expense) => {
+    setEditingExpense(expense);
+    setOpenModel();
+  };
+
   return (
-    <section>
+    <section className="container p-4 mx-auto">
       <TotalExpenses />
-      {/* <Expenses /> */}
-      <CreateExpense />
+      <Expenses onEditExpense={handleEditExpense} />
+
+      <CreateExpense
+        openModel={openModel}
+        expense={editingExpense}
+        onClose={handleCloseCreateModal}
+      />
     </section>
   );
 };
